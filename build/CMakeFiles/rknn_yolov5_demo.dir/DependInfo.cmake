3583a1e93792fbdbef1d3d0a56216fabdf1f4146
leoduan92@gmail.com <leoduan92@gmail.com>
2022-06-17 04:34:38 +0800

    first time push code

---
+++ build/CMakeFiles/rknn_yolov5_demo.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/drx/workshop/rknn_yolov5_opencv_demo/src/drm_func.c" "/home/drx/workshop/rknn_yolov5_opencv_demo/build/CMakeFiles/rknn_yolov5_demo.dir/src/drm_func.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../3rdparty/drm/include"
+  "../3rdparty/drm/include/libdrm"
+  "../include"
+  "../3rdparty/librknn_api/include"
+  "../3rdparty/opencv/include"
+  "../3rdparty/opencv/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/drx/workshop/rknn_yolov5_opencv_demo/src/main.cc" "/home/drx/workshop/rknn_yolov5_opencv_demo/build/CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o"
+  "/home/drx/workshop/rknn_yolov5_opencv_demo/src/postprocess.cc" "/home/drx/workshop/rknn_yolov5_opencv_demo/build/CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../3rdparty/drm/include"
+  "../3rdparty/drm/include/libdrm"
+  "../include"
+  "../3rdparty/librknn_api/include"
+  "../3rdparty/opencv/include"
+  "../3rdparty/opencv/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rknn_yolov5_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov5_demo.dir/src/drm_func.c.o"
+  "CMakeFiles/rknn_yolov5_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolov5_demo.dir/src/postprocess.cc.o"
+  "rknn_yolov5_demo"
+  "rknn_yolov5_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/rknn_yolov5_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
